30a4fd0fe956517d74b548fdc87cbd7d312d15d0
Siarhei Lapich <svlapich@gmail.com>
2020-02-06 23:11:45 +0300

    create-table.sql and add 2 table in database

---
+++ src/main/resources/create-table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE faculty (
+                         faculty_id   INTEGER NOT NULL AUTO_INCREMENT,
+                         faculty_name VARCHAR(45) NULL,
+                         number_of_applicant VARCHAR(45) NULL,
+                         PRIMARY KEY (faculty_id)
+);
+
+CREATE TABLE applicant (
+                           applicant_id    INTEGER NOT NULL AUTO_INCREMENT,
+                           firstname VARCHAR(45) NULL,
+                           lastname  VARCHAR(45) NULL,
+                           faculty_id   INTEGER NOT NULL,
+                           mark    DECIMAL(3,1) NOT NULL DEFAULT 0,
+                           PRIMARY KEY (applicant_id),
+                           CONSTRAINT applicant_to_faculty_fk
+                               FOREIGN KEY (faculty_id)
+                                   REFERENCES faculty (faculty_id)
+                                   ON DELETE NO ACTION
+                                   ON UPDATE NO ACTION
+);
